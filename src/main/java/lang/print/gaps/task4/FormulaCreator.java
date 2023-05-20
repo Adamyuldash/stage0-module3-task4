d1eca32c60cbe6662dab37cc3b46a740bc264834
Adam Yuldashev <adamyuldashev1997@gmail.com>
2023-05-20 09:31:53 -0700

    awesome!

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double sum;
+        double a = 2;
+        double b = 3;
+
+        sum = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+        System.out.println(sum);
+
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 99;
+        char s = 104;
+        char b = 97;
+        char a = 114;
+        System.out.println(c + s + b + a);
+
+
     }
 }
